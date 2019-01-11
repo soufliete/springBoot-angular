c450c4d1d11415b8abc4a90602196877ba89e412
soufliete <liete.kaka@gmail.com>
2019-01-11 19:01:38 +0100

    second commit

---
+++ demo/src/main/java/org/sid/dao/ContactRepository.java
@@ -0,0 +1,10 @@
+package org.sid.dao;
+
+import org.sid.entities.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, Long>{
+	
+	
+
+}
--- demo/src/main/java/org/sid/entities/Contact.java
+++ demo/src/main/java/org/sid/entities/Contact.java
@@ -1,5 +1,70 @@
 package org.sid.entities;
 
-public class Contact {
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Contact implements Serializable{
+	
+	@Id
+	@GeneratedValue
+	private long Id;
+	private String nom;
+	private String prenom;
+	@Temporal(TemporalType.DATE)
+	private Date dateNaissance;
+	private String email;
+	private long tel;
+	private String photo;
+	public long getId() {
+		return Id;
+	}
+	public void setId(long id) {
+		Id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getTel() {
+		return tel;
+	}
+	public void setTel(long tel) {
+		this.tel = tel;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	
 
 }
